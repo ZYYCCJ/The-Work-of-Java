c411656a2495e736ec2b15fe6d4727bb9baa908b
ZYYCCJ <1287910144@qq.com>
2021-10-08 16:21:36 +0800

    Create Welcom_With_Three_Messages.java

---
+++ src/test1/Welcom_With_Three_Messages.java
@@ -0,0 +1,9 @@
+package test1;
+
+public class Welcom_With_Three_Messages {
+	public static void main(String[] atgs) {
+		System.out.println("Programming is fun!");
+		System.out.println("Fundamentals First");
+		System.out.println("problem Driven");
+	}
+}
