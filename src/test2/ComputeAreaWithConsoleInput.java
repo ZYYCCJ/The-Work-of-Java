3a9120db3ef21178c2039b41dbd6f0640882ead4
ZYYCCJ <1287910144@qq.com>
2021-10-09 11:21:43 +0800

    Create ComputeAreaWithConsoleInput.java

---
+++ src/test2/ComputeAreaWithConsoleInput.java
@@ -0,0 +1,13 @@
+package test2;
+
+import java.util.Scanner;
+
+public class ComputeAreaWithConsoleInput {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a number foe radius: ");
+		double radius = input.nextDouble();
+		double area = radius * radius * 3.14159;
+		System.out.println("The area foe the circle of radius " + radius + " is "+ area);
+	}
+}
