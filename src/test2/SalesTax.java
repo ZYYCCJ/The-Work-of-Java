7138527d47256fb2076aedb92ba6a28d432368c5
ZYYCCJ <1287910144@qq.com>
2021-10-15 13:51:56 +0800

    Create SalesTax.java

---
+++ src/test2/SalesTax.java
@@ -0,0 +1,14 @@
+package test2;
+
+import java.util.Scanner;
+
+public class SalesTax {
+	public static void main(String[] args) {
+		try (Scanner input = new Scanner(System.in)) {
+			System.out.println("Enter purchase amount:");
+			double purchaseAmount = input.nextDouble();
+			double tax = purchaseAmount * 0.06;
+			System.out.println("Sales tax is $"+(int)(tax * 100) / 100.0);
+		}
+	}
+}
